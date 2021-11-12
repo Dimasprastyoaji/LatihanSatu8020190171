689ff5fc77fad41f09574f95f7c74a796a6f1861
robisb15 <robiaulia.k@gmail.com>
2021-11-12 23:03:37 +0700

    commit pertama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    8020190084 Robi Aulia Kurniawan
+                    Laravel
                 
                 </div>
 
